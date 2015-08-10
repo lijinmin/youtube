70396c5cc1f28ba353b87bff0f1912e909695fa6
KvApril <649047231@qq.com>
2015-08-10 11:56:06 +0800

    m

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -31,19 +31,14 @@ class AuthController < ApplicationController
             client_id: "687842974914-3i5h5geogqp2tu7anr8gf3rqqfc03h8s.apps.googleusercontent.com",
             client_secret: "ZnOGPGeQykb_XaTpPwyedwQ7",
             token_credential_uri: "https://accounts.google.com/o/oauth2/token",
-            grant_type: 'code',
+            grant_type: 'authorization_code',
             redirect_uri: "http://youtube.aggso.com/auth/oauth2callback")
 
-        # logger.info @code = Code.first
-        # logger client.code=(@code.code)
         client.code=(params[:code])
         logger.info client.grant_type
 
-        # client.fetch_access_token!
         logger.info res = client.fetch_access_token!
-        # @code.access_token = res["access_token"]
-        # @code.refresh_token = res["refresh_token"]
-        # @code.save
+  
         logger.info "***********************************"
         
 
