55e2e1923637d7cb16a8fbc7a051ad7a14bcfa7b
KvApril <649047231@qq.com>
2015-08-10 11:47:17 +0800

    m

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -19,25 +19,24 @@ class AuthController < ApplicationController
     def oauth2callback
         logger.info "***********************************"
         logger.info  params[:code]
-        if Code.first.blank?
-             @code = Code.new(code: params[:code])
-             @code.save
-         else
-            @code = Code.first
-            @code = @code.update(code: params[:code])
-        end
+        # if Code.first.blank?
+        #      @code = Code.new(code: params[:code])
+        #      @code.save
+        #  else
+        #     @code = Code.first
+        #     @code = @code.update(code: params[:code])
+        # end
 
          client = Signet::OAuth2::Client.new(
-        client_id: "687842974914-3i5h5geogqp2tu7anr8gf3rqqfc03h8s.apps.googleusercontent.com",
-        client_secret: "ZnOGPGeQykb_XaTpPwyedwQ7",
-        token_credential_uri: "https://accounts.google.com/o/oauth2/token",
-        grant_type: 'authorization_code',
-        redirect_uri: "http://youtube.aggso.com/auth/oauth2callback")
+            client_id: "687842974914-3i5h5geogqp2tu7anr8gf3rqqfc03h8s.apps.googleusercontent.com",
+            client_secret: "ZnOGPGeQykb_XaTpPwyedwQ7",
+            token_credential_uri: "https://accounts.google.com/o/oauth2/token",
+            grant_type: 'authorization_code',
+            redirect_uri: "http://youtube.aggso.com/auth/oauth2callback")
 
-        logger.info @code = Code.first
-        logger client.code=(@code.code)
-
-        logger.info client.client_id
+        # logger.info @code = Code.first
+        # logger client.code=(@code.code)
+        client.code=(params[:code])
         logger.info client.grant_type
 
         # client.fetch_access_token!
